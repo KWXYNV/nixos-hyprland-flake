89dbb94f018db3a90a7b07280335597c7771d2d2
KWXYNV <185575447+KWXYNV@users.noreply.github.com>
2024-12-07 02:34:44 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,14 @@
 {
-    description = "gpskwlkr NixOS";
+    description = "spy NixOS";
 
     inputs = {
 	    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
         nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
         home-manager.url = "github:nix-community/home-manager/release-23.11";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-        firefox-addons = {
-            url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        lanzaboote = {
-            url = "github:nix-community/lanzaboote/v0.3.0";
-
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
     };
 
-    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, lanzaboote, ... }@inputs:
+    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
 	let 
 	    lib = nixpkgs.lib;
 	    system = "x86_64-linux";
@@ -27,11 +16,10 @@
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
 	in
     {
-		nixosConfigurations.nixos-personal = lib.nixosSystem {
+		nixosConfigurations.nixos = lib.nixosSystem {
                 inherit system;
 				modules = [
-                    ./system/configuration.nix 
-                    lanzaboote.nixosModules.lanzaboote 
+                    ./system/configuration.nix  
 				];
                 specialArgs = {
                     inherit pkgs-unstable;
@@ -39,7 +27,7 @@
         };
 
 		homeConfigurations = {
-			gpskwlkr = home-manager.lib.homeManagerConfiguration {
+			spy = home-manager.lib.homeManagerConfiguration {
 				inherit pkgs;
 				modules = [ ./home ];
                 extraSpecialArgs = {
