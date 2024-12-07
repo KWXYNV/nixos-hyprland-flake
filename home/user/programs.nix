c8e8a56ee746cc43b996ea899ec7013dd44262c9
KWXYNV <185575447+KWXYNV@users.noreply.github.com>
2024-12-07 02:38:39 +0000

    Update programs.nix

--- home/user/programs.nix
+++ home/user/programs.nix
@@ -6,14 +6,9 @@
     profiles.spy = {
         extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
             bypass-paywalls-clean
-            darkreader
-            facebook-container
             i-dont-care-about-cookies
-            proton-pass
-            to-google-translate
-            view-image
             ublock-origin
-            youtube-shorts-block
+            
         ];
     };
   };
